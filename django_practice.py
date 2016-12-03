5277a64ddd7fa37115a41a91f858bb4fc88692d5
Driz <Driz@Drizs-MacBook-Pro.local>
2016-12-03 11:40:55 -0600

    added practice commands

--- django_practice.py
+++ django_practice.py
@@ -6,3 +6,11 @@ if 5 > 2:
     print ('5 is huge!')
 else:
     print ('5 is a little whimp')
+
+name = 'Sonia'
+if name == 'Ola':
+	print ('hey Ola')
+elif name == 'Sonia':
+	print ('hello Sonia')
+else:
+	print ('not today, satan')
